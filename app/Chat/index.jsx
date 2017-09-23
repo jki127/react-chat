cd89ad08ce7d05c3e74bf2cca4d22a9069437142
Jayson Isaac <jayson.isaac1@gmail.com>
2017-09-23 14:28:36 -0400

    Setup initial structure

---
+++ app/Chat/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Chat.css'
+
+class Chat extends React.Component {
+  render(){
+    return (
+      <div className="Chat">
+        <h1>Chat Heading</h1>
+      </div>
+    );
+  }
+}
+
+export default Chat;
