960a3e586f66a46f19e5e9f74957944c4118674b
Jayson Isaac <jayson.isaac1@gmail.com>
2017-09-30 22:20:27 -0400

    Add 'Interface' font

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
       }
     ]
   },
