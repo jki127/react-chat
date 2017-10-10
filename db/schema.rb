a193dc68f2f0d391d0c11febe72fa8b7c2356980
Jayson Isaac <jayson.isaac1@gmail.com>
2017-10-09 21:50:03 -0400

    mysql -> postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171007163640) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", null: false
